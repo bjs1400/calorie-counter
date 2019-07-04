0098888d2f788724fb532013993061a7e439d37c
Brittany <brittjsauce@yahoo.com>
2019-07-04 15:39:53 -0500

    save

--- js/index.js
+++ js/index.js
@@ -2,24 +2,31 @@ const deleteItem = id => {
   document.getElementById(id).style.display = "none";
 };
 
-const itemAdd = () => {
+const itemAdd = (itemName, numCalories) => {
+  if (!itemName || !numCalories) {
+    return false;
+  }
+
+  let newId = document.getElementsByTagName("tr").length.toString();
+
   var table = document.querySelector(".main-table");
   var row = document.createElement("tr");
+  row.setAttribute("id", newId);
 
-  var cell1 = document.createElement("td");
-  var cell1Text = document.createTextNode("1");
-  cell1.appendChild(cell1Text);
+  // var cell1 = document.createElement("td");
+  // var cell1Text = document.createTextNode(newId);
+  // cell1.appendChild(cell1Text);
 
   var cell2 = document.createElement("td");
   var cell2Text = document.createTextNode("+");
   cell2.appendChild(cell2Text);
 
   var cell3 = document.createElement("td");
-  var cell3Text = document.createTextNode("Item 1");
+  var cell3Text = document.createTextNode(itemName || "item Name");
   cell3.appendChild(cell3Text);
 
   var cell4 = document.createElement("td");
-  var cell4Text = document.createTextNode("65");
+  var cell4Text = document.createTextNode(numCalories || "num calories");
   cell4.appendChild(cell4Text);
 
   var cell5 = document.createElement("td");
@@ -27,24 +34,16 @@ const itemAdd = () => {
   cell5.appendChild(cell5Text);
 
   cell5.addEventListener("click", () => {
-    deleteItem("1");
+    deleteItem(newId);
   });
 
-  row.appendChild(cell1);
+  // row.appendChild(cell1);
   row.appendChild(cell2);
   row.appendChild(cell3);
   row.appendChild(cell4);
   row.appendChild(cell5);
 
   table.appendChild(row);
-};
 
-{
-  /* <tr id="1">
-  <th>1</th>
-  <th>+</th>
-  <th>Item 1</th>
-  <th>65</th>
-  <th onclick="deleteItem('1')">-</th>
-</tr> */
-}
+  return false;
+};
